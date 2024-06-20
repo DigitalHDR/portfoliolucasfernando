7f0dd5ea26fd44ff1226f9bdde9a3e3b3999d5d0
Lucas Fernando <73972922+DigitalHDR@users.noreply.github.com>
2024-06-19 22:04:53 -0300

    header

---
+++ src/app/config/navegacaoHeader/index.ts
@@ -0,0 +1,14 @@
+interface NagegacaoHeaderProps {
+  nome: string
+  href: string
+}
+
+const navegacaoHeader: NagegacaoHeaderProps[] = [
+  { nome: 'Perfil', href: 'perfil' },
+  { nome: 'Sobre', href: 'sobre' },
+  { nome: 'Skill', href: 'skill' },
+  { nome: 'Projetos', href: 'projetos' },
+  { nome: 'Certificados', href: 'certificados' },
+]
+
+export default navegacaoHeader
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,37 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss'
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+
+    // Or if using `src` directory:
+    './src/**/*.{js,ts,jsx,tsx}',
+    './node_modules/tw-elements/js/**/*.js',
   ],
   theme: {
+    // colors: {
+    //   primary: 'var(--primary)',
+    // },
     extend: {
+      keyframes: {
+        spin: {
+          '0%': { transform: 'rotate(0deg)' },
+          '100%': { transform: 'rotate(360deg)' },
+        },
+      },
+      animation: {
+        spin: 'spin 5s linear infinite',
+      },
       backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
     },
   },
-  plugins: [],
-};
-export default config;
+  darkMode: 'class',
+  plugins: [require('tw-elements/plugin.cjs')],
+}
+export default config
