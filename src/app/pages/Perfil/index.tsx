4a97953bf2574780e2eb1fffee8ac701bb43e84c
Lucas Fernando <73972922+DigitalHDR@users.noreply.github.com>
2024-06-30 23:23:37 -0300

    svg mouse com dark mode

--- src/app/pages/Perfil/index.tsx
+++ src/app/pages/Perfil/index.tsx
@@ -79,6 +79,7 @@ const Perfil: React.FC = () => {
       <div>
         <div className="flex justify-center mb-[65px]">
           <svg
+            className="stroke-black dark:stroke-white"
             width="26px"
             height="100%"
             viewBox="0 0 247 390"
@@ -91,12 +92,12 @@ const Perfil: React.FC = () => {
               className="animate-wheel"
               id="wheel"
               d="M123.359,79.775l0,72.843"
-              style={{ fill: 'none', stroke: '#000', strokeWidth: '20px' }}
+              style={{ fill: 'none', strokeWidth: '20px' }}
             ></path>
             <path
               id="mouse"
               d="M236.717,123.359c0,-62.565 -50.794,-113.359 -113.358,-113.359c-62.565,0 -113.359,50.794 -113.359,113.359l0,143.237c0,62.565 50.794,113.359 113.359,113.359c62.564,0 113.358,-50.794 113.358,-113.359l0,-143.237Z"
-              style={{ fill: 'none', stroke: '#000', strokeWidth: '20px' }}
+              style={{ fill: 'none', strokeWidth: '20px' }}
             ></path>
           </svg>
         </div>
