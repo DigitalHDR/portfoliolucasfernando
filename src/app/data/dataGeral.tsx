45ce77ecc61d26237defa8b8623b42d6ec52730e
Lucas Fernando <73972922+DigitalHDR@users.noreply.github.com>
2025-06-17 17:51:49 -0300

    url pokedex ativa

--- src/app/data/dataGeral.tsx
+++ src/app/data/dataGeral.tsx
@@ -20,9 +20,8 @@ const dataGeral: EspecificacaoProps = {
       type: 'react',
       img: 'https://i.postimg.cc/pVS2krDD/Captura-de-tela-2025-05-18-185622.png',
       title: 'Pokedex 3D',
-      desc: 'Next, Tailwind, Typescript',
-      url: '#',
-      // url: 'https://pokehdr3d.vercel.app/',
+      desc: 'EM FASE DE DESENVOLVIMENTO',
+      url: 'https://pokehdr3d.vercel.app/',
     },
     {
       id: 1,
